ef650aa07f13bcbaf5405539673e84b077077f2b
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2025-10-03 23:04:18 +0000

    added comments

---
+++ main.ts
@@ -0,0 +1,10 @@
+/**
+ * @author Zachary Fowler
+ * @version 1.0.0
+ * @date 2025-10-03
+ * @fileoverview This is the traditional "Hello, World!" program."
+ */
+// print out the greeting
+console.log("Hello, World!");
+
+console.log("\nDone.");
