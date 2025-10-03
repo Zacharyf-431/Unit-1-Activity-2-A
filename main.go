bc30d1c8201608bb925e4f83bd5f983b5857c6d1
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2025-10-03 23:14:34 +0000

    Added comments and traditional code

---
+++ main.go
@@ -0,0 +1,19 @@
+/*
+	* Author: Zachary Fowler
+	* Version: 1.0.0
+	* Date: 2025-10-3
+	* This is the traditional "Hello, World!" program.
+	*/
+
+
+
+package main
+
+import "fmt"
+
+func main() {
+	//print out the greeting
+	fmt.Println("Hello, World!")
+
+	fmt.Println("\nDone.")
+}
--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /**
- * @author Zachary Fowler
- * @version 1.0.0
- * @date 2025-10-03
- * @fileoverview This is the traditional "Hello, World!" program."
+ * @author: Zachary Fowler
+ * @version: 1.0.0
+ * @date: 2025-10-03
+ * @fileoverview: This is the traditional "Hello, World!" program."
  */
 // print out the greeting
 console.log("Hello, World!");
